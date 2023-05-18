8ad75c822e9f2ee6b018eb65e05dda4e1e893491
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-05-18 22:08:09 +0700

    Membenarkan Typo dan " ;  "

--- 103_singlelinkedlist/103_singlelinkedlist.cpp
+++ 103_singlelinkedlist/103_singlelinkedlist.cpp
@@ -121,7 +121,7 @@ void searchData() {
 		while (currentNode != NULL) {
 			if (currentNode->noMhs == nim) {
 				cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
-				return
+				return;
 			}
 			currentNode = currentNode->next;
 		}
@@ -189,5 +189,5 @@ int main() {
 			cout << "Terjadi Kesalahan" << endl;
 		}
 
-	}while (pilihan != 5)
+	} while (pilihan != 5);
 }
