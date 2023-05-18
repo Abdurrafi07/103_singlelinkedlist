6fe28ca09fd58d54134206f03dd29d524f7deae6
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-05-18 21:03:30 +0700

    Menambahkan fungsi perulangan if, else if, else pada bool serachNode

--- 103_singlelinkedlist/103_singlelinkedlist.cpp
+++ 103_singlelinkedlist/103_singlelinkedlist.cpp
@@ -59,4 +59,17 @@ bool serachNode(int nim, Node* current, Node previous) {
 		previous = current;
 		current = current->next;
 	}
+
+	if (current == NULL)
+	{
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
